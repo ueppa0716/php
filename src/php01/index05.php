796fa2cd011f4a88adde27e53db719a8ceb04223
ueppa <ueppa0716@gmail.com>
2024-02-05 20:40:35 +0900

    リモートリポジトリの変更

---
+++ src/php01/index05.php
@@ -0,0 +1,16 @@
+<?php
+$Fizz = "Fizz";
+$Buzz = "Buzz";
+$FizzBuzz = "FizzBuzz";
+
+for ($i = 1; $i <= 50; $i++) {
+  if ($i % 15 == 0) {
+    echo $FizzBuzz . "<br />";
+  } else if ($i % 3 == 0) {
+    echo $Fizz . "<br />";
+  } else if ($i % 5 == 0) {
+    echo $Buzz . "<br />";
+  } else {
+    echo $i . "<br />";
+  }
+}
